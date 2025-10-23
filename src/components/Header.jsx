9e797c1334a8e26b849550dc464bf4fce937fcf5
MeghanaKallepalli-Tech <meghana.k@station-s.org>
2025-10-23 12:34:26 +0530

    testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="app-header">
-      <h1>Vocabulary Hub</h1>
+      <h1>Vocabulary Hubs</h1>
     </header>
   );
 };
